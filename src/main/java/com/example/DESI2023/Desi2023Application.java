9a6372d663dc4ae1b58af80b01c9cb19b4e0138b
luuciooo <lucioabalos97@gmail.com>
2023-11-30 12:36:09 -0300

    primer commit

---
+++ src/main/java/com/example/DESI2023/Desi2023Application.java
@@ -0,0 +1,13 @@
+package com.example.DESI2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desi2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desi2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DESI2023/Desi2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DESI2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desi2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
